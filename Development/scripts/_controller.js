060a65c8c2e38148551c302a7e453a92215651e8
brettgris <brettgrisigner@gmail.com>
2015-08-27 12:14:11 -0600

    First Commit

---
+++ Development/scripts/_controller.js
@@ -0,0 +1,18 @@
+var Site = Site || {};
+
+(function ($, Site) {
+	Site.Controller = (function(){
+		/***************
+		INIT
+		****************/
+		function init(){
+		}
+		
+		/***************
+		PUBLIC
+		****************/
+		return {
+			init:init
+		}
+	})();
+})($,Site);
