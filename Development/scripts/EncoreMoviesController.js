babb6c247bef150535d8c258d6b0f69918357d66
brettgris <brettgrisigner@gmail.com>
2015-08-31 17:02:36 -0600

    Structure of files and Some styling

---
+++ Development/scripts/EncoreMoviesController.js
@@ -0,0 +1,221 @@
+(function(){
+	var app = angular.module('EncoreMovies',[]);
+	
+	/**************************
+	  MAIN CONTROLLER
+	**************************/
+	app.controller("microMoviesController", ['$scope', '$http', '$rootScope', '$location', '$timeout' , function($scope, $http, $rootScope, $location, $timeout) {
+		$http.get('data/movies.json').then(function(response) {
+			$scope.movies = response.data;
+		});
+	}]);
+	
+	/**************************
+	  DIRECTIVE FOR LIST
+	**************************/
+/*
+	app.directive('encoreMoviesList', EncoreMovieList); 
+	EncoreMovieList.$inject = ['$location', '$rootScope', '$timeout'];
+	function EncoreMovieList($location, $rootScope, $timeout) {
+		var directive = {
+			restrict: 'E',
+			templateUrl: 'templates/list.html'
+			//controller: EncoreMovieListController,
+			//link: EncoreMovieListLink
+		}
+		return directive;	
+	}
+*/
+	
+
+	
+	/**************************
+	  LINK FOR LIST
+	**************************/
+/*
+	function EncoreMovieListLink($scope, $element, $attrs) {
+		
+	}
+*/
+
+	
+	/**************************
+	  DIRECTIVE FOR SLIDER
+	**************************/
+	app.directive('encoreMoviesSlider', ['$location', '$rootScope', '$timeout', function($location, $rootScope, $timeout) {
+		var directive = {
+			restrict: 'A',
+			templateUrl: 'templates/slider.html',
+			link: function($scope, $element, $attrs) {
+				$scope.clickMovie = clickMovie;
+				$scope.closeExpander = closeExpander;
+				
+				$scope.expanderDetails = undefined;	
+				
+				$element.hide();
+				function waitForDom(){
+					$timeout(function(){
+						$element.show();
+						var $slides = $element.find(".movie-thumb");
+						$element.css("max-width", $slides.filter(":first").outerWidth() * $slides.length);
+						
+						$element.slick({
+                                slide: '.movie-thumb',
+                                slidesToShow: 9,
+                                slidesToScroll: 8,
+                                lazyLoad: 'ondemand',
+                                infinite: false,
+                                responsive: [
+                                	{
+										breakpoint: 2149,
+                                        settings: {
+                                            slidesToShow: 8,
+                                            slidesToScroll: 7
+                                        }
+                                    },{
+                                        breakpoint: 1919,
+                                        settings: {
+                                            slidesToShow: 7,
+                                            slidesToScroll: 6
+                                        }
+                                    },{
+                                        breakpoint: 1799,
+                                        settings: {
+                                            slidesToShow: 6,
+                                            slidesToScroll: 5
+                                        }
+                                    },{
+                                        breakpoint: 1544,
+                                        settings: {
+                                            slidesToShow: 5,
+                                            slidesToScroll: 4
+                                        }
+                                    },{
+                                        breakpoint: 1289,
+                                        settings: {
+                                            slidesToShow: 4,
+                                            slidesToScroll: 3
+                                        }
+                                    },{
+                                        breakpoint: 1034,
+                                        settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 2
+                                        }
+                                    },{
+                                        breakpoint: 779,
+                                        settings: {
+                                            slidesToShow: 2,
+                                            slidesToScroll: 1
+                                        }
+                                    },{
+                                        breakpoint: 640,
+                                        settings: {
+                                            slidesToShow: 4,
+                                            slidesToScroll: 3
+                                        }
+                                    }, {
+                                        breakpoint: 567,
+                                        settings: {
+                                            slidesToShow: 3,
+                                            slidesToScroll: 2
+                                        }
+                                    }, {
+                                        breakpoint: 473,
+                                        settings: {
+                                            slidesToShow: 2,
+                                            slidesToScroll: 1
+                                        }
+                                    }
+                                ],
+                            });
+					},50);
+				}
+				
+				waitForDom();	
+				
+				function clickMovie(movie, index) {
+	                $scope.previewIsActive = false;
+	                $scope.previewIsPlaying = false;
+	                $scope.previewIsLoading = false;
+	                $scope.previewIsWatched = false;
+	
+					if (movie.details) {
+						if ($scope.expanderDetails && movie.details.Title === $scope.expanderDetails.Title && movie.details.Description === $scope.expanderDetails.Description) {
+							
+						} else {
+							$scope.currentMovie = index;
+							$scope.activeRowIndex = index;
+							_onSliderSuccess(movie.details);
+						}
+						
+					} else {
+						$scope.isLoadingDetails = true;
+						$scope.currentMovie = index;
+						$scope.expanderDetails = {};
+						getmovieDetail(movie.id)
+					}
+					
+					$scope.activeRowIndex = $scope.rowIndex;
+	                $timeout(function() {
+	                    $scope.$apply();
+	                });
+	
+	                function getMovieDetail(_id) {
+		                console.log(_id);
+	                }
+	
+	                function _onSliderSuccess(detailsObj) {
+		                if (!movie.details) {
+	                        movie.details = detailsObj;
+	                    }
+	                    $scope.isLoadingDetails = false;
+	                    $scope.expanderDetails = detailsObj;
+	                    $scope.videoShareTitle = movie.details.title;
+	                    //$scope.videoShareUrl = $rootScope.getVideoUrl(movie.id, '', 'movie');
+	                    //$scope._previewAddPlayer();
+	                    //eventTrack("Expanded View", movie.details);
+	
+	                    if ($scope.addLocationHash) {
+	                        $location.replace().hash($element.prop("id"));
+	                    }
+	                }
+	
+	                function _onSliderError(error) {
+	                }
+	            }	
+	            
+	            function closeExpander() {
+		            $scope.currentMovie = -1;
+		            $scope.previewIsPlaying = false;
+		            $scope.previewIsWatched = false;
+		            //if (jwplayer("expander-preview-row-" + $scope.rowIndex).getState) {
+		                // jwplayer("expander-preview-row-" + $scope.rowIndex).setMute(true).stop();
+		            //}
+		        }
+			}
+		}
+		return directive;	
+	}]);
+	
+	
+	
+	/**************************
+	  DIRECTIVE FOR IMAGE
+	**************************/
+/*
+	app.directive('encoreMovieItem', ['$location', '$rootScope', '$timeout', function($location, $rootScope, $timeout) {
+		var directive = {
+			restrict: 'A',
+			link: function EncoreMovieItemLink($scope, $element, $attrs) {
+				$attrs.$observe('original', function(value){
+					$element.lazyload();
+				});
+			}
+		}
+		return directive;	
+	}]);
+*/
+	 
+	
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -5,56 +5,58 @@ var gulp = require('gulp');
 var concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
-	compass =  require('gulp-compass'),
-	browserify = require('gulp-browserify'),
 	gutil = require('gulp-util'),
 	browserSync = require('browser-sync').create(),
-	fileinclude = require('gulp-file-include'),
 	del = require('del'),
-	plumber = require('gulp-plumber');
-
-gulp.task('html', function() {
- 	gulp.src('Development/html/*.html')
-    .pipe(fileinclude('@@'))
-    .pipe(gulp.dest('Production/'))
-});
+	plumber = require('gulp-plumber'),
+	less = require('gulp-less'),
+	minifyCss = require('gulp-minify-css');
 
 // Compile Our Sass with Compass
-gulp.task('sass', function() {
-    return gulp.src('Development/scss/compile/*.scss')
+gulp.task('less', function() {
+    return gulp.src('Development/less/compile/*.less')
         .pipe(plumber({
 			errorHandler: function (error) {
 				console.log(error.message);
 				this.emit('end');
     	}}))
-        .pipe(compass({
-	        'sass': 'Development/scss/compile',
-	        'css': 'Production/css',
-	        'images': 'Production/images',
-	        'style': 'compressed'
-        }))
-        .on('error', function(err) {})
-        .pipe(gulp.dest('Production/css'));
-});
-
-gulp.task('css', ['sass'], function () {
-    del(['Production/css/**/*', '!Production/css/main.css']);
+    	.pipe(less())
+    	.on('error', function(err) {})
+    	//.pipe(minifyCss())
+    	.pipe(gulp.dest('Production/css'));
 });
 
 // JS PLUGINS - concat and min
 gulp.task('js', function() {
     return gulp.src('Development/scripts/*.js')
+    	.pipe(plumber({
+			errorHandler: function (error) {
+				console.log(error.message);
+				this.emit('end');
+    	}}))
         .pipe(concat('app.min.js'))
-        .pipe(browserify())
         .pipe(uglify())
+        .pipe(gulp.dest('Production/js'));
+});
+
+// JS PLUGINS - concat and min
+gulp.task('plugins', function() {
+    return gulp.src('Development/requirements/*.js')
+    	.pipe(plumber({
+			errorHandler: function (error) {
+				console.log(error.message);
+				this.emit('end');
+    	}}))
+        .pipe(concat('plugins.min.js'))
+        //.pipe(uglify())
         .pipe(gulp.dest('Production/js'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('Development/scripts/**/*', ['js']);
-    gulp.watch('Development/scss/**/*', ['sass']);
-    gulp.watch('Development/html/**/*', ['html']);
+    gulp.watch('Development/requirements/**/*', ['plugins']);
+    gulp.watch('Development/less/**/*', ['less']);
     gulp.watch(['Production/**/*']).on('change', browserSync.reload);
 });
 
@@ -68,4 +70,4 @@ gulp.task('sync', function() {
 });
 
 // Default Task
-gulp.task('default', ['html', 'sass', 'js', 'watch', 'sync']);
+gulp.task('default', ['less', 'js', 'plugins', 'watch', 'sync']);
